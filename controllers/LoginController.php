369d33e31cbd71e78ae15399b4a248557abdf648
xatier <xatierlike@gmail.com>
2013-04-03 23:35:26 +0800

    rename some APIs
    fix some urls

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -1,13 +1,12 @@
 <?php
    session_start(); 
-   
 
    $path1 = '../models/LoginCheck.php';
    $path2 = '../models/MysqliConnection.php';
    require_once("$path1"); 
    require_once("$path2"); 
    
-   $redirect_url = 'index.php';
+   $redirect_url = 'http://140.113.27.34:5566/index.php';
    $link = MysqliConnection('Read');
 
    //user input error detection and error message return
@@ -16,14 +15,14 @@
        $errmsg = 'Your ID format is wrong.';
    elseif(!CheckPasswd($_POST['passwd'])) 
        $errmsg = 'You have to enter your password.';
-   elseif(!CheckUserList_Id_and_Passwd($link, $_POST['account'], $_POST['passwd']))
+   elseif(!CheckUser_ID_and_Passwd($link, $_POST['account'], $_POST['passwd'])) {
        $errmsg = 'Login failed.';
-   else
-   {
-       $redirect_url = 'views/main.php';
+   }
+   else {
+       $redirect_url = 'http://140.113.27.34:5566/views/main.php';
        $_SESSION['account'] = $_POST['account'];
    }
 
    $_SESSION['errmsg'] = $errmsg;
-   header('Location: ../views/main.php');
+   header("Location: $redirect_url");
 ?>
