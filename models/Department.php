d05fac64d701579f6da77fff2351e9e515093e27
xatier <xatierlike@gmail.com>
2013-04-05 16:04:29 +0800

    add GetDepartmentList()
    fix the indent

--- models/Department.php
+++ models/Department.php
@@ -1,20 +1,43 @@
 <?php
-   function DepartListGen () {
-       require_once('../components/Mysqli.php');
-       $link = MysqliConnection('Read');
+function DepartListGen () {
+    require_once('../components/Mysqli.php');
+    $link = MysqliConnection('Read');
 
-       // check the Administrator table
-       $query = 'SELECT ID, Name FROM Department';
-       $stmt = mysqli_stmt_init($link);
-       if (mysqli_stmt_prepare($stmt, $query))
-       {
-           mysqli_stmt_execute($stmt);
-           mysqli_stmt_bind_result($stmt, $depart_id, $depart_name);
-           while(mysqli_stmt_fetch($stmt)) {
-               echo '<option value="'.$depart_id.'"> '.$depart_name.' </option>';
-           }
-           mysqli_stmt_close($stmt);
-       }
-   }
+    // get IDs and names from department
+    $query = 'SELECT ID, Name FROM Department';
+    $stmt = mysqli_stmt_init($link);
+    if (mysqli_stmt_prepare($stmt, $query))
+    {
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $depart_id, $depart_name);
+        while(mysqli_stmt_fetch($stmt)) {
+            echo '<option value="'.$depart_id.'"> '.$depart_name.' </option>';
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($link);
+}
+
+function GetDepartmentList() {
+    $link = MysqliConnection('Read');
+
+    // get department list
+    $query = 'SELECT Name FROM Department';
+    $stmt = mysqli_stmt_init($link);
+    $depart_list = array();
+    $depart_list_index = 1;
+    if (mysqli_stmt_prepare($stmt, $query))
+    {
+        mysqli_stmt_bind_param($stmt, "s", $id);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $depart);
+        while (mysqli_stmt_fetch($stmt)) {
+            $depart_list[$depart_list_index++] = $depart;
+        }
+        mysqli_stmt_close($stmt);
+    }
+    mysqli_close($link);
+    return $depart_list;
+}
 ?>
 
