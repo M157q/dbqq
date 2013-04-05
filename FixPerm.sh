783d71fd43a2d4d2c161c40f123f3820b5191153
xatier <xatierlike@gmail.com>
2013-04-06 00:47:03 +0800

    fix permission

--- FixPerm.sh
+++ FixPerm.sh
@@ -9,4 +9,4 @@ GROUP="team" # set to the right group name
 
 chown -R $USER:$GROUP $DIRS
 find $DIRS -type d -exec chmod 2775 {} \; # set the folder default permission
-find $DIRS -type f -exec chmod 2664 {} \; # set the file   default permission
+find $DIRS -type f -exec chmod 0664 {} \; # set the file   default permission
