ead96e1081d7695c435d5edbedf11e8c5063e848
xatier <xatierlike@gmail.com>
2013-04-05 12:55:14 +0800

    web pages for admin, professors, and students

---
+++ views/adm.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    $path = '../controllers/Session.php';
+    require_once("$path");
+    if(array_key_exists('id', $_SESSION))
+    {
+        echo 'user: ' . $_SESSION['id'] . ' has logined!!! <br />' ;
+        var_dump($_SESSION);
+        // check illegal users, and force to logout
+        if ($_SESSION['perm'] != 'adm') {
+            header('Location: '."../controllers/Logout.php");
+        }
+?>
+<!DOCTYPE html>
+<html lang="zh">
+    <head>
+        <meta charset="utf-8">
+        <title>Main</title>
+    </head>
+    <body>
+    <h1>安安我是 adm</h1>
+        <div>
+            <form name="logout" method="post" action="../controllers/Logout.php" >
+                <p>
+                    <input type="submit" value="登出" /><p>
+            </form>
+        </div>
+    </body>
+</html>
+<?php
+    }
+?>
