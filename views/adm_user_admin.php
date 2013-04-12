0e05c3efb88b496bfec81f915bf314ff1abf84f2
shunyi <M157q.tw@gmail.com>
2013-04-12 14:35:11 +0800

    add delete user form.

--- views/adm_user_admin.php
+++ views/adm_user_admin.php
@@ -8,20 +8,27 @@
 <html>
 <body>
 <?php echo "user admin"; ?>
-        <h2>更改使用者密碼</h2>
-        <hr>
-        <p> <form method="post" action="../controllers/AdmEditPass.php">
-            <label>密碼: (請輸入10個字以內的密碼)</label>
-            <input type="text" id="account" name="account" maxlength="10" placeholder="帳號" required />
-            <input type="password" id="passwd" name="new_passwd" maxlength="10" placeholder="新的密碼" required />
-            <input type="password" id="passwd" name="confirm_passwd" maxlength="10" placeholder="
+<hr>
+<h2>刪除使用者</h2>
+<p>
+<form id="delete" method="post" action="../controllers/AdmDeleteUser.php">
+    <input type="text" id="account" name="account" maxlength="10" placeholder="帳號" required />
+    <button form="delete" type="submit">刪除使用者</button>
+</form>
+</p>
+<hr>
+<h2>更改使用者密碼</h2>
+<p> 
+<form id="passwd" method="post" action="../controllers/AdmEditPass.php">
+    <label>密碼: (請輸入10個字以內的密碼)</label>
+    <input type="text" id="account" name="account" maxlength="10" placeholder="帳號" required />
+    <input type="password" id="passwd" name="new_passwd" maxlength="10" placeholder="新的密碼" required />
+    <input type="password" id="passwd" name="confirm_passwd" maxlength="10" placeholder="
 密碼確認" required />
-            <select name="perm" required />
-                <option value="stu"> 學生 </option>
-                <option value="pro"> 教授 </option>
-            </select>
-            <button type="submit">更改密碼</button>
-        </form> </p>
-        <a href="../views/adm.php">back</a>
+    <button form="passwd" type="submit">更改密碼</button>
+ </form>
+ </p>
+<hr>
+<a href="../views/adm.php">back</a>
 </body>
 </html>
