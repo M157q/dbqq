cd111336f43f49ecf29587b2577c7530889edd4c
shunyi <M157q.tw@gmail.com>
2013-04-26 14:53:14 +0800

    Remove the admin part

--- models/User.php
+++ models/User.php
@@ -137,27 +137,11 @@ function CheckUser_ID_and_Passwd($id, $passwd) {
     $passwd = salted($passwd);
 
     $result = false;
-    $adm_id = false;
     $pro_id = false;
     $stu_id = false;
 
-    // query three tables and get user's id
-    //  Administrator is now presented as a flag in the table Student and Professor.
-    //  Not a table anymore.
-    // check the Administrator table
-    //$query = 'SELECT ID FROM Administrator WHERE ID=? AND Password=SHA1(?)';
-    //$stmt = mysqli_stmt_init($link);
-    //if (mysqli_stmt_prepare($stmt, $query))
-    //{
-    //    mysqli_stmt_bind_param($stmt, "ss", $id, $passwd);
-    //    mysqli_stmt_execute($stmt);
-    //    mysqli_stmt_bind_result($stmt, $adm_id);
-    //    mysqli_stmt_fetch($stmt);
-    //    mysqli_stmt_close($stmt);
-    //}
-
     // check the Professor table
-    $query = 'SELECT ID FROM Professor WHERE ID=? AND Password=SHA1(?)';
+    $query = 'SELECT COUNT(ID) FROM Professor WHERE ID=? AND Password=SHA1(?)';
     $stmt = mysqli_stmt_init($link);
     if (mysqli_stmt_prepare($stmt, $query))
     {
@@ -169,7 +153,7 @@ function CheckUser_ID_and_Passwd($id, $passwd) {
     }
 
     // check the Student table
-    $query = 'SELECT ID FROM Student WHERE ID=? AND Password=SHA1(?)';
+    $query = 'SELECT COUNT(ID) FROM Student WHERE ID=? AND Password=SHA1(?)';
     $stmt = mysqli_stmt_init($link);
     if (mysqli_stmt_prepare($stmt, $query))
     {
@@ -182,11 +166,10 @@ function CheckUser_ID_and_Passwd($id, $passwd) {
     mysqli_close($link);
 
     // set user's permission in session
-    //if ($adm_id)   $_SESSION['perm'] = 'adm';
-    if ($pro_id)   $_SESSION['perm'] = 'pro';
-    if ($stu_id)   $_SESSION['perm'] = 'stu';
+    if ($pro_id) $_SESSION['perm'] = 'pro';
+    if ($stu_id) $_SESSION['perm'] = 'stu';
 
-    $result = ($adm_id or $pro_id or $stu_id);
+    $result = ($pro_id or $stu_id);
     return $result;
 }
 
