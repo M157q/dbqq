a706333a00da6ea12d5d6e47915d07f81d1a616b
xatier <xatierlike@gmail.com>
2013-05-01 20:04:17 +0800

    bootstrap adm_course_adm

--- views/adm_course_admin.php
+++ views/adm_course_admin.php
@@ -5,45 +5,54 @@
     require_once("../controllers/Session.php");
     require_once("../models/User.php");
     if ($_SESSION['adm'] == false) RedirectByPerm($_SESSION['perm']);
-    showWarning();
+        showWarning();
 ?>
-<html>
+<title>課程管理安安</title>
+<head>
+    <meta charset="utf-8">
+    <link href="../views/css/bootstrap.css" rel="stylesheet">
+</head>
 <body>
-<h1>課程管理</h1>
-<hr>
-<?php
-    require_once("../models/Course.php");
-    GetCourseInfoTable();
-?>
-<hr>
-<h2>刪除課程</h2>
-<p>
-<form id="delete_course" method="post" action="../controllers/AdmDeleteCourse.php">
-    <label>課號: </label>
-    <input type="text" id="course_id" name="course_id" placeholder="課號" required />
-    <label>年度: </label>
-    <input type="text" id="course_year" name="course_year" placeholder="年度" required />
-    <button form="delete_course" type="submit">刪除該課程</button>
-</form>
-</p>
-<hr>
-<h2>新增/刪除課程內學生</h2>
-<p>
-<form id="edit_course_stu" method="post" action="../controllers/AdmEditCourseTaken.php">
-    <p>
-    <input type="radio" name="action" value="add" required />新增<br />
-    <input type="radio" name="action" value="delete" required />刪除<br />
-    </p>
-    <label>課號: </label>
-    <input type="text" name="course_id" placeholder="課號" required />
-    <label>年度: </label>
-    <input type="text" name="course_year" placeholder="年度" required />
-    <label>學生帳號: </label>
-    <input type="text" name="stu_id" placeholder="學生帳號" required />
-    <button form="edit_course_stu" type="submit">確定</button>
-</form>
-</p>
-<hr>
-<a href="../index.php">back</a>
+    <div class="container">
+        <h1>課程管理</h1>
+        <hr>
+        <?php
+            require_once("../models/Course.php");
+            GetCourseInfoTable();
+        ?>
+        <hr>
+        <h2>刪除課程</h2>
+        <p>
+        <form id="delete_course" method="post" action="../controllers/AdmDeleteCourse.php">
+            <label>課號: </label>
+            <input type="text" id="course_id" name="course_id" placeholder="課號" required />
+            <label>年度: </label>
+            <input type="text" id="course_year" name="course_year" placeholder="年度" required />
+            <br>
+            <button class="btn btn-success" form="delete_course" type="submit">刪除該課程</button>
+        </form>
+        </p>
+        <hr>
+        <h2>新增/刪除課程內學生</h2>
+        <p>
+        <form id="edit_course_stu" method="post" action="../controllers/AdmEditCourseTaken.php">
+            <p>
+            <label class="radio">
+                <input type="radio" name="action" value="add" required />新增</label>
+            <label class="radio">
+                <input type="radio" name="action" value="delete" required />刪除</label>
+            </p>
+            <label>課號: </label>
+            <input type="text" name="course_id" placeholder="課號" required />
+            <label>年度: </label>
+            <input type="text" name="course_year" placeholder="年度" required />
+            <label>學生帳號: </label>
+            <input type="text" name="stu_id" placeholder="學生帳號" required />
+            <button form="edit_course_stu" type="submit">確定</button>
+        </form>
+        </p>
+        <hr>
+        <a class="btn btn-info" href="../index.php">back</a><br/>
+        <hr>
 </body>
 </html>
