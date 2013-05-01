2a5304a29014c62e462a0979d11913d8caa1fd1d
shunyi <M157q.tw@gmail.com>
2013-05-02 00:42:57 +0800

    edit the layout

--- index.php
+++ index.php
@@ -52,22 +52,31 @@
 
     <div class="container">
       <div class="masthead">
-        <h1  class="muted" text="#">歡迎使用選課系統</h1>
+        <h1  class="page-header" align="center">歡迎使用選課系統</h1>
       </div>
       <!-- Jumbotron -->
       <div class="jumbotron">
-        <h2>進擊の威儀</h2>
-        <p class="lead">If you see this page, our server is successfully working. ^q^</p>
-        <p class="lead">Your IP is: <?php echo $_SERVER['REMOTE_ADDR']; ?> </p>
-        <p></p><p></p><p></p><p></p>
+        <p class="codeblock">
+          <h3>部份功能有用到 html5 故有些瀏覽器可能無法支援:-(</h3>
+              <!-- Oh noes, you found it! -->
+              hey, <span style="position: absolute; left: -100px; top: -100px"> σ ﾟ∀ ﾟ) ﾟ∀ﾟ)σ  阿哈哈你看看你</span> try to copy this line XD
+        </p>
+        <div class="alert alert-success">
+            <p class="lead">If you see this page, our server is successfully working. ^q^</p>
+            <p class="lead">Your IP is: <?php echo $_SERVER['REMOTE_ADDR']; ?> </p>
+        </div>
+        <div class="alert alert-info">
          <!-- 進擊の威儀 -->
+        <h2>進擊の威儀</h2>
         <p><iframe width="853" height="480" src="https://www.youtube-nocookie.com/embed/YUMIoF8XGC4?rel=0&autoplay=1" frameborder="0" allowfullscreen style="margin: 0, auto"></iframe> </p>
+      </div>
       </div>
 
       <script>alert("助教安安，您累惹嗎？聽首歌吧")</script>
 
 <!-- login -->
 
+<div class="hero-unit">
 <form method="post" action="controllers/Login.php">
     <p>
         <label>帳號:</label>
@@ -87,15 +96,14 @@ if(isset($_SESSION['errmsg']) && !empty($_SESSION['errmsg'])){
     echo "$errormsg".'<br/>';
 }
 ?>
-        <p><a class="btn btn-success" href="/views/stu_regist.php">學生申請帳號</a></p>
-        <p><a class="btn btn-success" href="/views/pro_regist.php">教授申請帳號</a></p>
-        <p><a class="btn btn-success" href="/views/course_list.php">課程列表</a></p>
-        <h3>部份功能有用到 html5 故有些瀏覽器可能無法支援:-(</h3>
-        <p class="codeblock">
-              <!-- Oh noes, you found it! -->
-              hey, <span style="position: absolute; left: -100px; top: -100px"> σ ﾟ∀ ﾟ) ﾟ∀ﾟ)σ  阿哈哈你看看你</span>
-            try to copy this line XD
-            </p>
+    <hr>
+    <div class="row-fluid">
+        <a class="btn btn-success" href="/views/stu_regist.php">學生申請帳號</a>
+        <a class="btn btn-success" href="/views/pro_regist.php">教授申請帳號</a>
+        <a class="btn btn-success" href="/views/course_list.php">課程列表</a>
     </div>
+
+    </div>
+</div>
 </body>
 </html>
