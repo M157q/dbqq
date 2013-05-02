06dc15ec32b80ce77e9e5d8734eb1f0573b25b20
shunyi <M157q.tw@gmail.com>
2013-05-02 12:02:16 +0800

    Change the code alignment and add successful message

--- controllers/ProAddCourse.php
+++ controllers/ProAddCourse.php
@@ -13,7 +13,7 @@
     $errmsg = '';
     
     if($_POST['student_upper_bound'] < 1)
-	$errmsg = '人數上限必須大於零';
+        $errmsg = '人數上限必須大於零';
     elseif($_POST['credit'] < 0)
         $errmsg = '學分數不能為負值';
     elseif($_POST['day1'] == $_POST['day2'])
@@ -21,39 +21,36 @@
     elseif(!isset($_POST['d1']))
         $errmsg = '時段一之時間未填';
     elseif($_POST['day2'] != "0" && !isset($_POST['d2']))
-	$errmsg = '時段二之時間未填';
+        $errmsg = '時段二之時間未填';
     else {
-	// push class hour data into a string
-	$class_hours = $_POST['day1'];
-	$d1 = $_POST['d1'];
+        // push class hour data into a string
+        $class_hours = $_POST['day1'];
+        $d1 = $_POST['d1'];
         $day1 = array("0"=>"N", "1"=>"N", "2"=>"N", "3"=>"N", "4"=>"N", "5"=>"N", "6"=>"N", "7"=>"N");
-	foreach($d1 as $i => $x) { 
-            $day1[$x] = "Y";
-	}
-	$daystr = implode($day1);
+        foreach($d1 as $i => $x) $day1[$x] = "Y";
+        $daystr = implode($day1);
         $class_hours .= $daystr;
-	if($_POST['day2'] != "0") {
-	    $class_hours .= $_POST['day2'];
-	    $d2 = $_POST['d2'];
+        if($_POST['day2'] != "0") {
+            $class_hours .= $_POST['day2'];
+            $d2 = $_POST['d2'];
             $day2 = array("0"=>"N", "1"=>"N", "2"=>"N", "3"=>"N", "4"=>"N", "5"=>"N", "6"=>"N", "7"=>"N");
-            foreach($d2 as $i => $x) {
-                $day2[$x] = "Y";
-	    }
-	    $daystr = implode($day2);
-	    $class_hours .= $daystr;
-	}
-	if(CheckProIfCollision($_SESSION['id'], $class_hours))
-	    $errmsg = '此課程時段與其他課程衝堂';
-	else {
+            foreach($d2 as $i => $x) $day2[$x] = "Y";
+            $daystr = implode($day2);
+            $class_hours .= $daystr;
+        }
+
+        if(CheckProIfCollision($_SESSION['id'], $class_hours))
+            $errmsg = '此課程時段與其他課程衝堂';
+        else {
             $name = $_POST['name'];
             $class_room = $_POST['class_room'];
-	    $student_upper_bound = $_POST['student_upper_bound'];
-	    $required = $_POST['required'];
-	    $credit = $_POST['credit'];
+            $student_upper_bound = $_POST['student_upper_bound'];
+            $required = $_POST['required'];
+            $credit = $_POST['credit'];
             $year = $_POST['year'];
             $department = $_POST['department'];
             $grade = $_POST['grade'];
-	    $additional_info = $_POST['additional_info'];
+            $additional_info = $_POST['additional_info'];
 
             // insert the data to the database
             $link = MysqliConnection('Write');
@@ -61,14 +58,15 @@
             $stmt = mysqli_stmt_init($link);
             if (mysqli_stmt_prepare($stmt, $query))
             {
-	        mysqli_stmt_bind_param($stmt, "dssdsddssss", $year, $name, $_SESSION['id'], $student_upper_bound, $class_room, $credit, $required, $department, $grade, $class_hours, $additional_info);
+                mysqli_stmt_bind_param($stmt, "dssdsddssss", $year, $name, $_SESSION['id'], $student_upper_bound, $class_room, $credit, $required, $department, $grade, $class_hours, $additional_info);
                 mysqli_stmt_execute($stmt);
                 mysqli_stmt_close($stmt);
             }
             mysqli_close($link);
             // if adding course is ok, then goto the professor page
             $redirect_url = '../views/pro.php';
-	}
+            $errmsg = '您已成功新增課程!';
+        }
     }
 
     $_SESSION['errmsg'] = $errmsg;
