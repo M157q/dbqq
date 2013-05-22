8ab15294739ad9def72f3f1d9609625151860a55
shunyi <M157q.tw@gmail.com>
2013-05-22 13:34:02 +0800

    add pro delete stu function

---
+++ controllers/ProDeleteStu.php
@@ -0,0 +1,33 @@
+<?php session_start();
+    require_once('../controllers/Session.php');
+    require_once('../models/User.php');
+    require_once('../components/Mysqli.php');
+
+    CheckPermAndRedirect($_SESSION['perm'], 'pro');
+
+    $id = $_POST['account'];
+    $errmsg = '';
+    list($course_id, $course_year) = explode('_', $_SESSION['id_year']);
+
+    if (!CheckStudentIDExist($id)) {
+        $errmsg = "此學生帳號 $id 並不存在";
+    }
+    else {
+        // update the data in the database
+        $link = MysqliConnection('Write');
+        $query = 'DELETE FROM Course_taken WHERE StudentID=? AND CourseID=? AND CourseYear=?';
+        $stmt = mysqli_stmt_init($link);
+        if (mysqli_stmt_prepare($stmt, $query)) {
+            mysqli_stmt_bind_param($stmt, "sss", $id, $course_id, $course_year);
+            if (mysqli_stmt_execute($stmt)) $errmsg = '您已成功刪除該使用者';
+            else $errmsg = "$id 不在本課程的修課學生名單內";
+            mysqli_stmt_close($stmt);
+        }
+        mysqli_close($link);
+    }
+    
+
+    $_SESSION['errmsg'] = $errmsg;
+    //RedirectByPerm($_SESSION['perm']);
+    header('Location: ../views/pro_view_student.php');
+?>
--- views/pro_view_student.php
+++ views/pro_view_student.php
@@ -23,19 +23,33 @@
     require_once("../models/User.php");
     require_once("../components/Mysqli.php");
     require_once("../controllers/Session.php");
+    
     if (isBanned($_SESSION['id'])) RedirectByPerm($_SESSION['perm']);
     if ($_SESSION['perm'] != 'pro') RedirectByPerm($_SESSION['perm']);
-    showWarning();
-    if(!isset($_POST['id_year'])) {
+    
+    if(isset($_POST['id_year'])) $_SESSION['id_year'] = $_POST['id_year'];
+    if(!isset($_SESSION['id_year'])) { 
         $_SESSION['errmsg'] = '沒有選擇課程 無法顯示學生列表';
         RedirectByPerm($_SESSION['perm']);
     }
+
+    showWarning();
     
-    ProViewCourseStudent($_POST['id_year']);
+    ProViewCourseStudent($_SESSION['id_year']);
 ?>
+        <div class="form-actions">
+        <h2>刪除本課程學生</h2>
+        <p><form id="delete" method="post" action="../controllers/ProDeleteStu.php">
+            <label>帳號: </label>
+            <input type="text" id="account" name="account" maxlength="10" placeholder="帳號" required />
+            <br>
+            <button class="btn btn-danger" form="delete" type="submit">刪除</button>
+        </form></p>
+        </div>
+
         <a class="btn btn-info" href="./pro.php">回上頁</a>
+        </div>
     </div>
-</div>
 </body>
 </html>
 
